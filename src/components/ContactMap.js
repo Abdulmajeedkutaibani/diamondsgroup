4abf5489f82bfdc7f85d5358ef57ac8169b40c20
Abdulmajeed.K <77694213+Abdulmajeedkutaibani@users.noreply.github.com>
2023-02-27 14:39:19 +0300

    Update ContactMap.js to new location

--- src/components/ContactMap.js
+++ src/components/ContactMap.js
@@ -12,7 +12,7 @@ export const ContactMap = () => {
 
 function Map() {
   const center = useMemo(
-    () => ({ lat: 41.008135631028225, lng: 28.951109307810082 }),
+    () => ({ lat: 41.0124505, lng: 28.9401294 }),
     []
   );
 
